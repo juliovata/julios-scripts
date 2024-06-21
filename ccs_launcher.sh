a4d749e5337d13705c44cc4c764a6bd62bdc6080
juliovata <jvata@umich.edu>
2024-06-20 21:00:29 -0400

    first commit

---
+++ ccs_launcher.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+GTK_THEME=Adwaita:light /home/jvata/Programs/ti/ccs1270/ccs/eclipse/ccstudio
---
+++ stm32_launcher.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+GTK_THEME=Adwaita:light /opt/st/stm32cubeide_1.15.1/stm32cubeide_wayland $1
